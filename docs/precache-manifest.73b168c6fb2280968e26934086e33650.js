9fdb7491d87f5af654697eb47630f2fc082dbf6b
Juan Ignacio <juaniclausi@gmail.com>
2021-04-21 22:25:51 -0300

    docs Agregados

---
+++ docs/precache-manifest.73b168c6fb2280968e26934086e33650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6e23b8b8ad5f01bfac4ea090a4a393",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0494d5a8344b0ecf7e89",
+    "url": "/static/css/main.794fcfe9.chunk.css"
+  },
+  {
+    "revision": "f152750524e089b32b63",
+    "url": "/static/js/2.1db71e64.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.1db71e64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0494d5a8344b0ecf7e89",
+    "url": "/static/js/main.8988a171.chunk.js"
+  },
+  {
+    "revision": "c47b2b6426a52a18ac50",
+    "url": "/static/js/runtime-main.b71dd995.js"
+  }
+]);
